a8be8b9f3f0559c946405d9b7baef74e07e7a7fb
Adha Rasyid <adharasyid2005@gmail.com>
2024-11-25 12:11:12 +0700

    versi-3

--- laithan_git.py
+++ laithan_git.py
@@ -1,4 +1,12 @@
 print("Hello World!")
 
 import random
-print("Random number: ", random.randint(1, 10))
+import os
+
+guess = "Random number: ", random.randint(1, 10)
+inpt = input("Enter a number between 1 and 10: ")
+
+if int(inpt) == guess:
+    print("You got it!")
+else:
+    print("You didn't get it! Try again.")
